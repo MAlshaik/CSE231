a515b24bd0febfa555a5b9348f8afd384ac60c7b
MAslahik <workmail2776@gmail.com>
2022-10-09 18:16:47 -0400

    done

--- lab05B.py
+++ lab05B.py
@@ -17,12 +17,12 @@ for i in range(len(Names)):
     Names[i] = Names[i].replace(" ", '')
 
 file.seek(0)
-Title = file.readlines(0)[0].strip() 
+Title = file.readlines(0)[0].strip() + "  BMI        "
 
-print(Title)
-print(Height)
-print(Weight)
-print(Names)
+# print(Title)
+# print(Height)
+# print(Weight)
+# print(Names)
 
 maxHeight = 0
 minHeight = 99999
@@ -30,11 +30,21 @@ minWeight = 99999
 maxWeight = 0
 avgHeight = 0
 avgWeight = 0 
+maxBMI = 0
+minBMI = 99999
+avgBMI = 0
 for i in range(len(Height)):
 
     BMI.append(round(float(Weight[i])/float(Height[i])**2, 2))
     Height[i] = float(Height[i]) 
     Weight[i] = float(Weight[i])
+
+    if(BMI[i] > maxBMI):
+        maxBMI = BMI[i]
+
+    if(BMI[i] < minBMI):
+        minBMI = BMI[i]
+
     if(float(Height[i]) > maxHeight):
         maxHeight = float(Height[i])
 
@@ -49,33 +59,32 @@ for i in range(len(Height)):
 
     avgHeight += float(Height[i])
     avgWeight += float(Weight[i])
+    avgBMI += float(BMI[i])
 
 avgHeight = round(avgHeight /  len(Height), 2)
 avgWeight = round(avgWeight / len(Weight), 2)
-maxBMI = round((maxWeight)/(maxHeight)**2,2)
-minBMI = round(minWeight/minHeight**2,2)
-avgBMI = round(avgWeight/avgHeight**2, 2)
+avgBMI = round(avgBMI/len(BMI), 2)
 
-print(BMI)
-print(f" max height: {maxHeight}\n min Height: {minHeight} \n max Weight: {maxWeight}\n min Weight: {minWeight}")
-print(f"min BMI {minBMI} max BMI {maxBMI}")
-print(f"avg BMI {avgBMI}")
+
 
 file.seek(0)
-file.close()
+
+
+
 
 newfile = open('output.txt', 'w')
-newfile.writelines([Title,"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[0], Height[0], Weight[0], BMI[0]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[1], Height[1], Weight[1], BMI[1]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[2], Height[2], Weight[2], BMI[2]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[3], Height[3], Weight[3], BMI[3]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[4], Height[4], Weight[4], BMI[4]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[5], Height[5], Weight[5], BMI[5]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[6], Height[6], Weight[6], BMI[6]),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format(Names[7], Height[7], Weight[7], BMI[7]),
-"\n",
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Average",avgHeight, avgWeight, avgBMI),
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Max",maxHeight, maxWeight, maxBMI), 
-"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Min",minHeight, minWeight, minBMI)])
-newfile.close()
+newfile.writelines((Title,
+f"\n{Names[0]:<12s}{Height[0]:<12.2f}{Weight[0]:<12.2f}{BMI[0]:<11.2f}",
+f"\n{Names[1]:<12s}{Height[1]:<12.2f}{Weight[1]:<12.2f}{BMI[1]:<11.2f}",
+f"\n{Names[2]:<12s}{Height[2]:<12.2f}{Weight[2]:<12.2f}{BMI[2]:<11.2f}",
+f"\n{Names[3]:<12s}{Height[3]:<12.2f}{Weight[3]:<12.2f}{BMI[3]:<11.2f}",
+f"\n{Names[4]:<12s}{Height[4]:<12.2f}{Weight[4]:<12.2f}{BMI[4]:<11.2f}",
+f"\n{Names[5]:<12s}{Height[5]:<12.2f}{Weight[5]:<12.2f}{BMI[5]:<11.2f}",
+f"\n{Names[6]:<12s}{Height[6]:<12.2f}{Weight[6]:<12.2f}{BMI[6]:<11.2f}",
+f"\n{Names[7]:<12s}{Height[7]:<12.2f}{Weight[7]:<12.2f}{BMI[7]:<12.2f}",
 
+
+"\n\n{:<12s}{:<12.2f}{:<12.2f}{:<11.2f}".format("Average",avgHeight, avgWeight, avgBMI),
+"\n{:<12s}{:<12.2f}{:<12.2f}{:<11.2f}".format("Max",maxHeight, maxWeight, maxBMI), 
+"\n{:<12s}{:<12.2f}{:<12.2f}{:<12.2f}".format("Min",minHeight, minWeight, minBMI)))
+file.close()
