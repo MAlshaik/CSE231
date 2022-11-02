9ed4068b1d2ee6eb7a289fd69210321da4b49daa
MAslahik <workmail2776@gmail.com>
2022-11-02 14:39:50 -0400

    Reordered some files

--- scratchpaper.py
+++ scratchpaper.py
@@ -1,19 +1,7 @@
-# from proj07 import highest_rated_by_movie
-
-# N = 10
-# L_reviews = [[], [(2, 3), (3, 3), (6, 1), (9, 5), (10, 2)], 
-#              [(3, 1), (4, 4), (5, 3), (6, 5), (7, 3), (8, 5)], 
-#              [(1, 2), (4, 3), (7, 3)], [(10, 5)], [],
-#              [(2, 2), (5, 2), (8, 3)], [], [],
-#              [(2, 2), (10, 4)], []]
-# L1 = [1,2,3,6,7]
-
-# L_max, max_score = highest_rated_by_movie(L1,L_reviews,N)
-# print("Student L_max:",L_max)
-# print("Student max score:",max_score)
-# assert max_score == 3.0
-# assert L_max == [6, 7]
-avg = [0, 2.0, 2.3333333333333335, 2.0, 0, 0, 3.0, 3.0, 0, 0, 0]
-index = [j for j in range(len(avg)) if avg[j] == max(avg)]
-
-print(index)
+genres_num = [1,0,0,1,0,1,1,1,0,0,0,1]
+for i in range(2):
+    for i in range(len(genres_num)):
+        if i == 0:
+            print('hi')
+    
+    genres_num.append(1)
