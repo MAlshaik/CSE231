2c6db45bdea1053fcbc3f0d8cd03ecc57e4f0476
N0H3dy <workmail2776@gmail.com>
2022-11-04 22:36:51 -0400

    Project 7 comments

--- scratchpaper.py
+++ scratchpaper.py
@@ -1,19 +1,28 @@
-# from proj07 import highest_rated_by_movie
+import string
+def build_wordlist(fp):
+    lines = fp.readlines()
+    words = []
+    for i in lines:
+        punc = string.punctuation
+        w = i.strip().split(' ')
+        for x in w:
+            new_x = x.translate(str.maketrans('', '', string.punctuation))
+            words.append(new_x)
+    return words
 
-# N = 10
-# L_reviews = [[], [(2, 3), (3, 3), (6, 1), (9, 5), (10, 2)], 
-#              [(3, 1), (4, 4), (5, 3), (6, 5), (7, 3), (8, 5)], 
-#              [(1, 2), (4, 3), (7, 3)], [(10, 5)], [],
-#              [(2, 2), (5, 2), (8, 3)], [], [],
-#              [(2, 2), (10, 4)], []]
-# L1 = [1,2,3,6,7]
+def find_unique(l):
+    x = []
+    for i in l:
+        if i not in x:
+            x.append(i)
+    return x
 
-# L_max, max_score = highest_rated_by_movie(L1,L_reviews,N)
-# print("Student L_max:",L_max)
-# print("Student max score:",max_score)
-# assert max_score == 3.0
-# assert L_max == [6, 7]
-avg = [0, 2.0, 2.3333333333333335, 2.0, 0, 0, 3.0, 3.0, 0, 0, 0]
-index = [j for j in range(len(avg)) if avg[j] == max(avg)]
+def main():
+    infile = open("test.txt", 'r') 
+    word_list = build_wordlist(infile)    
+    new_wordlist = find_unique(word_list)
+    new_wordlist.sort()
+    print(new_wordlist)
 
-print(index)
+if __name__ == "__main__":
+    main()
